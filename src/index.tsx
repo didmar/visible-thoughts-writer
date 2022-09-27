543a3dc3bc9bdb7f8ae1c45b5be4c6d7723df675
Didier Marin <mail@didiermarin.com>
2022-09-27 22:24:23 +0200

    Add StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
