fd86ee40f15e80f26cc1ccdbb31012e34f03b256
Didier Marin <mail@didiermarin.com>
2022-09-28 09:24:09 +0200

    Redirection when creating run

--- src/components/RunsPane.tsx
+++ src/components/RunsPane.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Run, getRuns, createRun } from '../firebase-app';
 import '../App.css';
 import { Button } from '@mui/material';
 
 function RunsPane(): JSX.Element {
-  const [runs, setRuns] = useState<Run[]>([]);
-  const [newTitle, setNewTitle] = useState('');
+  const [runs, setRuns] = useState<Run[] | undefined>(undefined);
+  const [newTitle, setNewTitle] = useState<string>('');
+  const [newRunId, setNewRunId] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     void (async function () {
@@ -26,16 +27,22 @@ function RunsPane(): JSX.Element {
         <Button
           variant="contained"
           onClick={() => {
-            void async function () {
-              await createRun(newTitle);
-            };
+            void (async function () {
+              console.log('newTitle ', newTitle);
+              const runId = await createRun(newTitle);
+              console.log('runId ', newTitle);
+              setNewRunId(runId);
+            })();
           }}
         >
           Create Run
+          {newRunId !== undefined && (
+            <Navigate replace to={`/runs/${newRunId}`} />
+          )}
         </Button>
-        <h1>Runs:</h1>
+        <h1>{runs !== undefined ? 'Runs:' : 'Loading...'}</h1>
         <ul>
-          {runs.map((run) => (
+          {runs?.map((run) => (
             <li key={run.id}>
               <Link to={`/runs/${run.id}`}>{run.title}</Link>
             </li>
