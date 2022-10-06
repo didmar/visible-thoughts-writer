2b96946089a6e9c684a250d807be061e92e6bd48
Didier Marin <mail@didiermarin.com>
2022-10-06 16:54:00 +0200

    chore: setup cloud functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // 'eslint:recommended',
+    // 'plugin:import/errors',
+    // 'plugin:import/warnings',
+    // 'plugin:import/typescript',
+    // 'google',
+  ],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {},
+};
---
+++ functions/src/index.ts
@@ -0,0 +1,1 @@
+// Write cloud function here
