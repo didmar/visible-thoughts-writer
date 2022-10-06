cb157ba98f8607f37510bd201e6a3c24b5b53dc6
Didier Marin <mail@didiermarin.com>
2022-10-06 11:13:30 +0200

    feat: add test case for steps merge

--- src/__tests__/firebase-app.spec.ts
+++ src/__tests__/firebase-app.spec.ts
@@ -1,26 +1,35 @@
 import { mergeStepsWithUpdates, Step } from '../firebase-app';
 
 test('mergeStepsWithUpdates', () => {
-  const sample1: Step[] = [new Step(1), new Step(2), new Step(3)];
-  const sample2: Step[] = [new Step(4), new Step(5), new Step(6)];
-  const sample3: Step[] = [new Step(1, [])];
+  const s123: Step[] = [new Step(1), new Step(2), new Step(3)];
+  const s456: Step[] = [new Step(4), new Step(5), new Step(6)];
+  const s1up: Step[] = [new Step(1, [])];
+  const s641up5: Step[] = [
+    new Step(6),
+    new Step(4),
+    new Step(1, []),
+    new Step(5),
+  ];
 
   expect(mergeStepsWithUpdates([], [])).toEqual([]);
-  expect(mergeStepsWithUpdates(undefined, sample1)).toEqual(sample1);
-  expect(mergeStepsWithUpdates(sample1, [])).toEqual(sample1);
-  expect(mergeStepsWithUpdates(sample1, sample2)).toEqual([
-    ...sample1,
-    ...sample2,
-  ]);
+  expect(mergeStepsWithUpdates(undefined, s123)).toEqual(s123);
+  expect(mergeStepsWithUpdates(s123, [])).toEqual(s123);
+  expect(mergeStepsWithUpdates(s123, s456)).toEqual([...s123, ...s456]);
   // Order is preserved
-  expect(mergeStepsWithUpdates(sample2, sample1)).toEqual([
-    ...sample1,
-    ...sample2,
-  ]);
-  expect(mergeStepsWithUpdates(sample1, sample3)).toEqual([
+  expect(mergeStepsWithUpdates(s456, s123)).toEqual([...s123, ...s456]);
+  expect(mergeStepsWithUpdates(s123, s1up)).toEqual([
     new Step(1, []),
     new Step(2),
     new Step(3),
   ]);
-  expect(mergeStepsWithUpdates(sample3, sample1)).toEqual(sample1);
+  expect(mergeStepsWithUpdates(s1up, s123)).toEqual(s123);
+  // Initial order does not matter, result will be sorted
+  expect(mergeStepsWithUpdates(s123, s641up5)).toEqual([
+    new Step(1, []),
+    new Step(2),
+    new Step(3),
+    new Step(4),
+    new Step(5),
+    new Step(6),
+  ]);
 });
